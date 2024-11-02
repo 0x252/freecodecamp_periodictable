3b9212c9d73a4ac0d015bfc24cd8677e19d3bb1d
Alexander Y <startyourday60@gmail.com>
2024-11-02 07:45:33 +0000

    fix: fix position to text output

--- element.sh
+++ element.sh
@@ -9,6 +9,11 @@ if [[ ! $1 =~ ^[0-9]+$ ]]; then
 else
  export element=$($PSQL "SELECT * FROM elements LEFT JOIN properties using(atomic_number) WHERE atomic_number='$1';")
 fi
+if [[ -z $element ]];
+then
+echo "I could not find that element in the database."
+else
 #IFS='|' echo $element | read -l atomic_number elemnt
 IFS='|' read -r atomic_number element_name element_symbol type atomic_mass melting_point boiling_point _ <<< "$element"
-echo "The element with atomic number $atomic_number is $element_name ($element_symbol). It's a $type, with a mass of $atomic_mass amu. $element_name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
+echo "The element with atomic number $atomic_number is $element_symbol ($element_name). It's a $type, with a mass of $atomic_mass amu. $element_name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
+fi
